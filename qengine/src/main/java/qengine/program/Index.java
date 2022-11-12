ff62ce67fd908fac72ac1e88841964d13eb094af
Ravenn2203 <lea.garcia2203@outlook.fr>
2022-11-12 18:17:22 +0100

    6 Index hexastore

---
+++ qengine/src/main/java/qengine/program/Index.java
@@ -0,0 +1,64 @@
+package qengine.program;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.eclipse.rdf4j.query.algebra.In;
+
+
+//Dès qu'on lit un nouveau triplet on actualise tous les index
+public class ManipulerIndex {
+
+    
+    
+    public ManipulerIndex(){}
+
+
+    //Méthode qui met un triplet dans l'index demandé
+    public void addTriplet(ArrayList<Integer> triplet, HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> index){
+        
+        //Si ils n'existent pas, ces variables ne sont pas ré-affectées donc nouvelle hashMapArrayList et nouvelle arrayList
+        HashMap<Integer, ArrayList<Integer>> hashMapArrayList = new HashMap<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        //Je récupère le HashMap etc associé au premier entier du triplet
+        for(Integer key : index.keySet()) {
+            if(key == triplet.get(0)){
+                //Si on entre ici c'est que le premier entier du triplet du triplet existe dans l'index (première colonne)
+                hashMapArrayList = index.get(key);
+            }
+        }
+
+        //Je récupère l'ArrayList associée au deuxième élément du triplet
+        for(Integer key : hashMapArrayList.keySet()) {
+            if(key == triplet.get(1)){
+                //Si on entre ici c'est que le deuxième entier du triplet du triplet existe dans l'index (deuxième colonne)
+                arrayList = hashMapArrayList.get(key);
+            }
+        }
+
+        //J'ajoute dans l'ArrayList le dernier élément du triplet
+        //Soit ça met le premier élément, soit ça ajoute à la suite
+        arrayList.add(triplet.get(2));
+
+        //J'ajoute à l'hashMapArrayList le deuxième + l'arrayList
+        //Soit ça ajoute pour la première fois soit ça remplace l'ancienne occurence
+        hashMapArrayList.put(triplet.get(1), arrayList);
+
+        //On ajoute à notre index la permutation du triplet
+        //Soit il n'existe pas et on l'ajoute soit on le met à jour
+        index.put(triplet.get(0), hashMapArrayList);
+
+        //On affiche l'état de l'index qui vient d'être mis à jour avec le nouveau tuple
+        System.out.println(index);
+
+    }
+
+
+    public HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> getIndexSPO() {
+        return indexSPO;
+    }
+
+    
+
+}
--- qengine/src/main/java/qengine/program/MainRDFHandler.java
+++ qengine/src/main/java/qengine/program/MainRDFHandler.java
@@ -1,5 +1,6 @@
 package qengine.program;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.eclipse.rdf4j.model.Statement;
@@ -16,31 +17,115 @@ import org.eclipse.rdf4j.rio.helpers.AbstractRDFHandler;
  * </p>
  */
 public final class MainRDFHandler extends AbstractRDFHandler {
-	
+
+	//Dictionnaire qui contient des clés Integer et valeur String. Chaque entier représente une ressource String contenue dans des triplets
 	public HashMap<Integer, String> dictionary = new HashMap<Integer, String>();
-	
+
+	//Classe qui va effectuer des opération sur les 6 index nécessaires dans l'approche hexastore, créer les fichiers représentant ces index, permettre l'ajout/suppression de ressources
+	public ManipulerIndex classeManipulationIndex = ManipulerIndex.getInstance();
+
+	//Nos 6 indexs
+	private HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> indexSPO = new HashMap<>();
+    private HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> indexSOP = new HashMap<>();
+    private HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> indexPSO = new HashMap<>();
+    private HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> indexPOS = new HashMap<>();
+    private HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> indexOSP = new HashMap<>();
+    private HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> indexOPS = new HashMap<>();
+		
 	public int cle = 0;
 
 	@Override
 	public void handleStatement(Statement st) {
+
+		//ArrayList qui stocke les clés correspondant aux String du triplet dont on s'occupe
+		ArrayList<Integer> keyList = new ArrayList<>();
+
 		System.out.println("\n" + st.getSubject() + "\t " + st.getPredicate() + "\t " + st.getObject());
 		
-		if(!dictionary.containsValue(String.valueOf(st.getSubject()))){
+		if(!dictionary.containsValue(String.valueOf(st.getSubject()))) {
 			dictionary.put(cle, String.valueOf(st.getSubject()));
+			keyList.add(cle);
 			cle++;
+		}else{
+			//Pour récupérer la clé associée à la ressource qui a déjà été ajoutée
+			for(HashMap.Entry<Integer, String> entry : dictionary.entrySet()) {
+				if(entry.getValue().equals(String.valueOf(st.getSubject()))){
+					//On ajoute cette clé au triplet
+					keyList.add(entry.getKey());
+				}
+			}
 		}
 		
 		if(!dictionary.containsValue(String.valueOf(st.getPredicate()))){
 			dictionary.put(cle, String.valueOf(st.getPredicate()));
+			keyList.add(cle);
 			cle++;
+		}else{
+			//Pour récupérer la clé associée à la ressource qui a déjà été ajoutée
+			for(HashMap.Entry<Integer, String> entry : dictionary.entrySet()) {
+				if(entry.getValue().equals(String.valueOf(st.getPredicate()))){
+					//On ajoute cette clé au triplet
+					keyList.add(entry.getKey());
+				}
+			}
 		}
 		
 		if(!dictionary.containsValue(String.valueOf(st.getObject()))){
 			dictionary.put(cle, String.valueOf(st.getObject()));
+			keyList.add(cle);
 			cle++;
+		}else{
+			//Pour récupérer la clé associée à la ressource qui a déjà été ajoutée
+			for(HashMap.Entry<Integer, String> entry : dictionary.entrySet()) {
+				if(entry.getValue().equals(String.valueOf(st.getObject()))){
+					//On ajoute cette clé au triplet
+					keyList.add(entry.getKey());
+				}
+			}
 		}
+
+		System.out.println(dictionary.toString());
+		System.out.println("________________________________________________________");
+
 		
-		//System.out.println(dictionary.toString());
+
+		ArrayList<Integer> keyListSOP = new ArrayList<>();
+		keyListSOP.add(keyList.get(0));
+		keyListSOP.add(keyList.get(2));
+		keyListSOP.add(keyList.get(1));
+		
+		ArrayList<Integer> keyListPSO = new ArrayList<>();
+		keyListPSO.add(keyList.get(1));
+		keyListPSO.add(keyList.get(0));
+		keyListPSO.add(keyList.get(2));
+
+		ArrayList<Integer> keyListPOS = new ArrayList<>();
+		keyListPOS.add(keyList.get(1));
+		keyListPOS.add(keyList.get(2));
+		keyListPOS.add(keyList.get(0));
+
+		ArrayList<Integer> keyListOSP = new ArrayList<>();
+		keyListOSP.add(keyList.get(2));
+		keyListOSP.add(keyList.get(0));
+		keyListOSP.add(keyList.get(1));
+		
+		ArrayList<Integer> keyListOPS = new ArrayList<>();
+		keyListOPS.add(keyList.get(2));
+		keyListOPS.add(keyList.get(1));
+		keyListOPS.add(keyList.get(0));
+		
+		System.out.println("\nIndex SPO :");
+		classeManipulationIndex.addTriplet(keyList, indexSPO);
+		System.out.println("\nIndex SOP :");
+		classeManipulationIndex.addTriplet(keyListSOP, indexSOP);
+		System.out.println("\nIndex PSO :");
+		classeManipulationIndex.addTriplet(keyListPSO, indexPSO);
+		System.out.println("\nIndex POS :");
+		classeManipulationIndex.addTriplet(keyListPOS, indexPOS);
+		System.out.println("\nIndex OSP :");
+		classeManipulationIndex.addTriplet(keyListOSP, indexOSP);
+		System.out.println("\nIndex OPS :");
+		classeManipulationIndex.addTriplet(keyListOPS, indexOPS);
 		
 	};
 }
